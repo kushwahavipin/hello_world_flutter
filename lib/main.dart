0f867798f8bffd3bb34bb97bd343ccfee8bf88b8
VIPIN KUSHWAHA <102202599+kushwahavipin@users.noreply.github.com>
2022-07-27 12:58:16 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+void main()=>  // fat arrow are use to less lines of code
+runApp(MaterialApp(
+  debugShowCheckedModeBanner: false, // Remove Debug batch banner
+  title: "My App",
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text("My First App"), // Change App Bar Title
+      backgroundColor: Colors.green, // Change App Bar Background Color
+    ),
+    body: Material(
+      color: Colors.blue, // Change Background Color of body
+      child: Center(
+        child: Text(
+          "Hello World",
+          textDirection: TextDirection.ltr, // Give Text Direction
+          style: TextStyle(
+              fontSize: 40.0, // Change fontSize in always float
+              color: Colors.white // Change text Color
+          ),
+        ),
+      ),
+    ),
+  ),
+));
+
